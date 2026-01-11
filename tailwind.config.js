3deb28eb04a221ba4eecb0696e3cc0deb9c86da4
thisisarchway <alyssa@thisisarchway.com>
2026-01-11 18:58:33 -0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       fontFamily: {
         'sans': ['Inter', 'sans-serif'],
+        'accent': ['Codystar', 'cursive'],
       },
       fontWeight: {
         'thin': '100',
